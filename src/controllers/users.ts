c01bcd8b2d8d24a979a34a580195aaf2445953a9
Marta Prosniak-Kalwaj <manj@gft.com>
2023-02-28 13:23:14 +0100

    DB scripts, fix ts config

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,18 +2,13 @@ import { Request, Response } from "express";
 import prisma from "../prisma";
 
 export const getUser = async (req: Request, res: Response) => {
-  console.log("Getting user");
-
   const { id } = req.params;
-  console.log(id);
-
+  
   const user = await prisma.user.findUnique({
     where: {
       id: Number(id),
     },
   });
-
-  console.log(user);
 
   res.status(200).json({ data: user });
 };
