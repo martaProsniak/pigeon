076488809964f17829128c46ca273d2a2f192d94
Marta Prosniak-Kalwaj <manj@gft.com>
2023-03-02 14:51:04 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+Express.js REST api exercise using TypeScript and Prisma
+
+## Dependencies
+
+First, please install the dependencies with `npm install`
+
+## Database
+
+###### Creating database locally
+
+Please run `npx prisma migrate dev --name init` to create your local database. For more information, please see [Prisma Doc](https://www.prisma.io/docs/getting-started/quickstart#3-run-a-migration-to-create-your-database-tables-with-prisma-migrate)
+
+###### Seeding test data
+
+To seed test data in your local database, please run `npm run db-seed`
+
+###### Database viewer
+
+To see the current database records in GUI, you can run `npm run db-studio`
+
+## Running app
+
+To start the app, please run `npm dev`
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,6 +6,7 @@ const userData: Prisma.UserCreateInput[] = [
   {
     name: "Michael Scott",
     email: "michael.scott@mspc.com",
+    // unhashed password: 'abc' - use to log in as a test user
     password: "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
     tweets: {
       create: [
@@ -19,11 +20,13 @@ const userData: Prisma.UserCreateInput[] = [
   {
     name: "Pam Beasley",
     email: "pam.beasley@mspc.com",
+    // unhashed password: 'def' - use to log in as a test user
     password: "cb8379ac2098aa165029e3938a51da0bcecfc008fd6795f401178647f96c5b34",
   },
   {
     name: "Ryan Howard",
     email: "ryan.howard@mspc.com",
+    // unhashed password: 'ghi' - use to log in as a test user
     password: "50ae61e841fac4e8f9e40baf2ad36ec868922ea48368c18f9535e47db56dd7fb",
     tweets: {
       create: [
